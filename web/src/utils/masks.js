189c78072167159e63c7bb2473b4cfdd2a2b281c
Filipe Batista <filipe.x@live.com>
2023-04-13 08:20:48 -0300

    removendo arquivos antigos

--- web/src/utils/masks.js
+++
@@ -1,8 +0,0 @@
-
-export function celular(value) {
-  value = value.replace(/\D/g, "");
-  value = value.replace(/^(\d{2})(\d)/g, "($1) $2");
-  value = value.replace(/(\d)(\d{4})$/, "$1-$2");
-
-  return value;
-}
