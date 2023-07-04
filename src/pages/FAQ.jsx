7cccf4b7c35c91cac5c940725d2af2f1eb998bb3
Filipe Batista <filipe.x@live.com>
2023-07-04 20:40:32 -0300

    update version

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -67,7 +67,7 @@ function FaqComponent() {
 
   function handleDownloadApp() {
     console.log("Baixar o app")
-    window.open(window.location.href + 'WhatsAppSenderBot.Setup.4.7.8.exe', '_blank')
+    window.open(window.location.href + 'WhatsAppSenderBot.Setup.4.8.0.exe', '_blank')
   }
 
   const loadFaqs = async () => {
